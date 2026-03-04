bab8a16628710546b04860f46d360351cf1f9dd2
Aiswarya V <aiswaryapunnad@gmail.com>
2026-03-04 13:05:26 +0530

    Use Render PostgreSQL

--- database.py
+++ database.py
@@ -1,18 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# PostgreSQL connection
-DATABASE_URL = "postgresql://postgres:Aishu%40123@localhost:5432/yc_intelligence"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Create engine
 engine = create_engine(DATABASE_URL)
 
-# Session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Base model
 Base = declarative_base()
+
